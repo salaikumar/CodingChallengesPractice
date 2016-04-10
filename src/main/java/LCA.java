473bccda1a74096d439abd08148360c0344f213c
Salaikumar <saravanan1115@gmail.com>
2016-04-10 12:02:28 +0530

    Adding few more problems to solve

---
+++ src/main/java/LCA.java
@@ -0,0 +1,17 @@
+
+/*
+* Least Common Ancestor of a Binary Search tree
+*/
+
+import java.util.*;
+
+public class LCA{
+  /*
+  * Get the path to the nodes and put it in seperate Stack
+  * Now, pop from the Stack and compare
+  */
+  public TreeNode(Node root, Node first, Node second){
+    Stack<Node> firstNodePath;
+    Stack<Node> secondNodePath;
+  }
+}
---
+++ src/main/java/ZigZagArray.java
@@ -0,0 +1,9 @@
+/*
+*  Given an Array, Arrange it in ZigZag manner
+*/
+
+public class ZigZagArray{
+  public List<Integer> sortZZ(List<Integer> input){
+    
+  }
+}
