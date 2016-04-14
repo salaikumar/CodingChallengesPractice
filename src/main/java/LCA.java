e1e6ae63e3ea1f1b16e458d40f2c7688de4eeab7
Salaikumar <saravanan1115@gmail.com>
2016-04-14 12:58:19 +0530

    LCA onBST, accepted solution on leetcode

--- src/main/java/LCA.java
+++ src/main/java/LCA.java
@@ -10,8 +10,18 @@ public class LCA{
   * Get the path to the nodes and put it in seperate Stack
   * Now, pop from the Stack and compare
   */
-  public TreeNode(Node root, Node first, Node second){
-    Stack<Node> firstNodePath;
-    Stack<Node> secondNodePath;
+  public TreeNode lca(TreeNode root, TreeNode first, TreeNode second){
+    if(root == null)
+      return null;
+
+    if (root.val > first.val && root.val > second.val) {
+      return lca(root.left,first,second);
+    }
+
+    if(root.val < first.val && root.val < second.val){
+      return lca(root.right, first, second);
+    }
+
+    return root;
   }
 }
