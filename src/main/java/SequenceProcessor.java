40590fad0f0ff579050d856cea1f2bf61ea1da03
Salaikumar <saravanan1115@gmail.com>
2016-04-19 07:42:43 +0530

    Moving Sum Digit from Util to main folder. Added Pblms solved yesterday w.o solution. Need to complete it

---
+++ src/main/java/SequenceProcessor.java
@@ -0,0 +1,20 @@
+/*
+* Problems on streams of 1 and 0s
+* Few problems solved yesterday. Need to implement and push it
+*/
+
+public class SequenceProcessor{
+
+  public int findLongestSequence(int[] bits){
+    return 0;
+  }
+
+  public void checkDivisibleByThree(){
+
+  }
+
+  public int findCelebrity(int[][] relationships){
+    return 1;
+  }
+
+}
